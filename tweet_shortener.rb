0bbbd4b88b06f56e66c98bfa8c0ea65498abc59d
Eliot Howes <ejhowes@gmail.com>
2019-06-01 15:34:33 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -46,10 +46,8 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  tweets.each do |tweet|
     if tweet.length > 140
         puts word_substituter(tweet)
       else puts tweet
     end
-  end
 end
