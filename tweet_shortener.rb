8ff6345959fd07099ff44569fba0ca17ea545c3f
Eliot Howes <ejhowes@gmail.com>
2019-06-01 13:14:51 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,11 +1,26 @@
 # Write your code here.
+# dictionary = {
+# :hello => "hi",
+# :to => {:two => {:too => "2"}},
+# :for => {:four => "4"},
+# :be => "b",
+# :you => "u",
+# :at => "@",
+# :and => "&"
+# }
 
 dictionary = {
 :hello => "hi",
-:to => {:two => {:too => "2"}},
-:for => {:four => "4"},
+:to => "2",
+:two => "2",
+:too => "2",
+:for => "4",
+:four => "4",
 :be => "b",
 :you => "u",
 :at => "@",
 :and => "&"
 }
+
+
+
