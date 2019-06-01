7a8547e6c38b9365a0245e9e6caf3fc954310919
Eliot Howes <ejhowes@gmail.com>
2019-06-01 14:05:50 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -13,17 +13,18 @@ require 'pry'
 
 def dictionary
   conversions = {
-:hello => "hi",
-:to => "2",
-:two => "2",
-:too => "2",
-:for => "4",
-:four => "4",
-:be => "b",
-:you => "u",
-:at => "@",
-:and => "&"
-}
+    :hello => "hi",
+    :to => "2",
+    :two => "2",
+    :too => "2",
+    :for => "4",
+    :four => "4",
+    :be => "b",
+    :you => "u",
+    :at => "@",
+    :and => "&"
+    }
+end
 
 def word_substituter(tweet)
   tweet_array = tweet.split
